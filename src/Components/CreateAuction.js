f88c5dc3c553e9b237526af74e52b9ad4f85a580
Caroline Riesten <riesten@hotmail.com>
2019-03-29 14:45:33 +0100

    install datepicker

--- src/Components/CreateAuction.js
+++ src/Components/CreateAuction.js
@@ -2,9 +2,22 @@ import React,{Component} from 'react';
 
 
 export default class CreateAuction extends Component{
+    constructor(props){
+        super(props)
+    }
 
+    handleSubmit(){
+
+
+    }
 render(){
 
-    return(<p></p>); 
+    return(<form onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Titel"/><br/>
+        <input type="text" placeholder="Beskrivning"/><br/>
+        <input type="text" placeholder="Utropspris"/><br/>
+        <input type="text" placeholder="Skapad av"/>
+        <button>Spara</button>
+    </form>); 
 }
 }
