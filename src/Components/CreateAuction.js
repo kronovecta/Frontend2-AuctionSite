ff00ca944f1c6ed396b0f59839ffd157d497e2b2
Schlebbe <sebastian@temtron.se>
2019-04-02 11:27:35 +0200

    Fixed setState for datepicker

--- src/Components/CreateAuction.js
+++ src/Components/CreateAuction.js
@@ -22,19 +22,29 @@ export default class CreateAuction extends Component {
         }
     }
 
-    handleChange = (e) => {
-        this.setState({
-            auction: {
-                ...this.state.auction,
-                [e.target.id]: e.target.value
-            }
-        })
+    handleChange = (date, id) => {
+        if (typeof date.target !== 'undefined') {
+            this.setState({
+                auction: {
+                    ...this.state.auction,
+                    [date.target.id]: date.target.value
+                }
+            })
+        }
+        else {
+            this.setState({
+                auction: {
+                    ...this.state.auction,
+                    [id]: date
+                }
+            })
+        }
     }
 
     handleSubmit = (e) => {
         e.preventDefault();
         let object = this.state.auction;
-        // postData(object, "auktion");
+        postData(object, "auktion");
         //TODO api create auction, call method postData()
     }
 
@@ -42,8 +52,8 @@ export default class CreateAuction extends Component {
         return (<form onSubmit={this.handleSubmit}>
             <input type="text" placeholder="Titel" id="Titel" onChange={this.handleChange} /> <br />
             <input type="text" placeholder="Beskrivning" id="Beskrivning" onChange={this.handleChange} /><br />
-            <DatePicker selected={this.state.auction.StartDatum} id="StartDatum" onSelected={this.handleSelected} />
-            <DatePicker selected={this.state.auction.SlutDatum} id="SlutDatum" onSelected={this.handleSelected} /><br />
+            <DatePicker selected={this.state.auction.StartDatum} onChange={(event) => {this.handleChange(event, "StartDatum")}} />
+            <DatePicker selected={this.state.auction.SlutDatum} onChange={(event) => {this.handleChange(event, "SlutDatum")}} /><br />
             <input type="text" placeholder="Utropspris" id="Utropspris" onChange={this.handleChange} /><br />
             <input type="text" placeholder="Skapad av" id="SkapadAv" onChange={this.handleChange} />
             <button>Spara</button>
