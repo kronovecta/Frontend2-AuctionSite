f0937c8510111ec8978a6fe9794e72c7c0230102
Caroline Riesten <riesten@hotmail.com>
2019-04-03 11:03:43 +0200

    bidlist

--- src/Components/BidList.js
+++ src/Components/BidList.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react'
+import { createSession, getSession } from '../api';
 
 export default class BidList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allBids: []
+    }
+  }
+  componentDidMount() {
+    createSession("bidList", "bud", this.props.selected.id).then(
+      this.setState({
+        allBids: getSession("bidList")
+      })
+    );
+
+  }
   render() {
+
+    let allTheBids = this.state.allBids.map((item) => {
+      return <div>
+        <tr>
+          <td>{item.Summa}</td> <td>{item.Budgivare}</td>
+        </tr>
+      </div>
+    });
     return (
       <div>
         <h3>BidList</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Bud</th>
+              <th>Budgivare</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allTheBids}
+          </tbody>
+        </table>
       </div>
     )
   }
