56d1fac096fa1a1e218b03f9d0d6798bda2efef4
Schlebbe <sebastian@temtron.se>
2019-04-03 14:59:11 +0200

    Api, CreateAuction

--- src/Components/CreateAuction.js
+++ src/Components/CreateAuction.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import { postData } from '../api';
+import { postData, updateData } from '../api';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 export default class CreateAuction extends Component {
@@ -17,10 +17,18 @@ export default class CreateAuction extends Component {
                 Beskrivning: "",
                 StartDatum: new Date(),
                 SlutDatum: new Date(),
-                Utropspris: 0,
+                Utropspris: null,
                 SkapadAv: "",
                 Gruppkod: 2050
             }
+        }
+    }
+
+    componentDidMount() {
+        if (typeof this.props.data != 'undefined') {
+            this.setState({
+                auction: this.props.data
+            })
         }
     }
 
@@ -45,53 +53,51 @@ export default class CreateAuction extends Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
-        let object = this.state.auction;
-        postData(object, "auktion");
-        //TODO api create auction, call method postData()
+        if (typeof this.props.data != 'undefined') {
+            updateData(this.state.auction, "auktion");
+        } else {
+            let object = this.state.auction;
+            postData(object, "auktion");
+        }
     }
-
 
     render() {
         return (
-            <div style={{display:'flex', justifyContent:'center'}}>
-                <form onSubmit={this.handleSubmit} style={{width:'50%'}}>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <form onSubmit={this.handleSubmit} style={{ width: '50%' }}>
                     <div className="form-group">
-                   
                         <label>Title</label>
-                        <input type="text" placeholder="Titel" id="Titel" onChange={this.handleChange} style={{width:'100%'}} />
+                        <input type="text" placeholder="Titel" id="Titel" onChange={this.handleChange} style={{ width: '100%' }} value={this.state.auction.Titel} />
                     </div>
-                    
+
                     <div className="form-group">
                         <label>Description</label>
-                       
-                        <textarea id="Beskrivning" onChange={this.handleChange} style={{width:'100%', marginB: '1rem'}} />
+                        <textarea id="Beskrivning" onChange={this.handleChange} style={{ width: '100%', marginB: '1rem' }} value={this.state.auction.Beskrivning} />
                     </div>
 
-                    <div style={{'display':'flex', justifyContent:'flex-start'}} className="form-group">
-                        <div style={{marginRight:'1rem', flex:'2'}}>
+                    <div style={{ 'display': 'flex', justifyContent: 'flex-start' }} className="form-group">
+                        <div style={{ marginRight: '1rem', flex: '2' }}>
                             <label>Created by</label>
-                            <input type="text" id="SkapadAv" onChange={this.handleChange} style={{width:'100%'}} />
+                            <input type="text" id="SkapadAv" onChange={this.handleChange} style={{ width: '100%' }} value={this.state.auction.SkapadAv} />
                         </div>
-                        <div style={{flex:'1'}}>
+                        <div style={{ flex: '1' }}>
                             <label>Starting price</label>
-                            <input type="number" placeholder="0" id="Utropspris" onChange={this.handleChange} style={{width:'100%'}} />
+                            <input type="number" placeholder="0" id="Utropspris" onChange={this.handleChange} style={{ width: '100%' }} value={this.state.auction.Utropspris} />
                         </div>
                     </div>
 
-                    <div style={{'display':'flex', justifyContent:'space-between'}} className="form-group">
-                        <div style={{marginRight:'1rem'}}>
-                            <div style={{display:'inline-block', marginRight: '1rem'}}>
-                                <label style={{display:'block'}}>Start date:</label>
-                              
-                                <DatePicker selected={this.state.auction.StartDatum} id="StartDatum" onChange={(event) =>{this.handleChange(event, "StartDatum")}} className="datePicker"/>
+                    <div style={{ 'display': 'flex', justifyContent: 'space-between' }} className="form-group">
+                        <div style={{ marginRight: '1rem' }}>
+                            <div style={{ display: 'inline-block', marginRight: '1rem' }}>
+                                <label style={{ display: 'block' }}>Start date:</label>
+                                <DatePicker selected={this.state.auction.StartDatum} id="StartDatum" onChange={(event) => { this.handleChange(event, "StartDatum") }} className="datePicker" value={this.state.auction.StartDatum} />
                             </div>
-                            <div style={{display:'inline-block'}}>
-                                <label style={{display:'block'}}>End date:</label>
-                              
-                                <DatePicker selected={this.state.auction.SlutDatum} id="SlutDatum" onChange={(event) => {this.handleChange(event, "SlutDatum")}}  className="datePicker"/>
+                            <div style={{ display: 'inline-block' }}>
+                                <label style={{ display: 'block' }}>End date:</label>
+                                <DatePicker selected={this.state.auction.SlutDatum} id="SlutDatum" onChange={(event) => { this.handleChange(event, "SlutDatum") }} className="datePicker" value={this.state.auction.SlutDatum} />
                             </div>
                         </div>
-                    
+
                         <button className="btn btn-primary">Spara</button>
                     </div>
 
--- src/Components/SingleAuction.js
+++ src/Components/SingleAuction.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react'
 import BidList from './BidList'
 import { deleteData } from '../api';
 import Moment from 'react-moment';
+import CreateAuction from './CreateAuction';
 
 export default class SingleAuction extends Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            displayAuction: true
+        }
+        this.handleClick = this.handleClick.bind(this);
     }
-    
+
+    handleClick() {
+        const newState = this.state.displayAuction === true ? false : true;
+        this.setState({ displayAuction: newState });
+    }
+
     render() {
-        
+
         let auctionItemStyle = {
             border: '1px solid lightgrey',
             borderRadius: '0.5rem',
@@ -18,71 +29,74 @@ export default class SingleAuction extends Component {
         }
 
         let auctionItemStyleHover = {
-            background:'red'
+            background: 'red'
         }
 
         let innerAuctionItemStyle = {
             padding: '1.5rem 2rem',
-            flex:'2',
-            marginRight:'2rem',
-            display:'flex',
-            flexDirection:'column',
-            justifyContent:'space-between'
+            flex: '2',
+            marginRight: '2rem',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between'
         }
 
-        return (
-            <div style={{display:'flex', justifyContent:'space-between', flexDirection:'row', flexWrap:'wrap'}}>
+        const Content = (
+            <div style={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'row', flexWrap: 'wrap' }}>
                 <div style={innerAuctionItemStyle}>
                     <div>
-                        <div style={{display:'flex', justifyContent: 'space-between', marginBottom: '1rem'}}>
-                            <h3 style={{marginBottom: '1rem', display:'inline-block'}}>{this.props.data.Titel}</h3>
-                            <p style={{textAlign:'right', display:'inline-block'}}>Utgångspris: <br/><span style={{fontWeight: '600'}}>{this.props.data.Utropspris} SEK</span></p>
+                        <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '1rem' }}>
+                            <h3 style={{ marginBottom: '1rem', display: 'inline-block' }}>{this.props.data.Titel}</h3>
+                            <p style={{ textAlign: 'right', display: 'inline-block' }}>Utgångspris: <br /><span style={{ fontWeight: '600' }}>{this.props.data.Utropspris} SEK</span></p>
                         </div>
-                        <p style={{padding:'1rem 0 1.5rem 0', borderTop:'1px solid lightgrey', borderBottom:'1px solid lightgrey'}}>{this.props.data.Beskrivning}</p>
+                        <p style={{ padding: '1rem 0 1.5rem 0', borderTop: '1px solid lightgrey', borderBottom: '1px solid lightgrey' }}>{this.props.data.Beskrivning}</p>
                         {/* <p style={{margin:'0'}}>Start: {this.props.data.StartDatum.substring(0,10)}</p> */}
                         {/* <p style={{margin:'0'}}>Slut: {this.props.data.SlutDatum.substring(0,10)}</p> */}
-                        
-                        <div style={{display:'block'}}>
-                            <p style={{display:'inline-block'}}>Start date: </p><Moment format="LLL" date={this.props.data.StartDatum} />
+
+                        <div style={{ display: 'block' }}>
+                            <p style={{ display: 'inline-block' }}>Start date: </p><Moment format="LLL" date={this.props.data.StartDatum} />
                         </div>
-                        <div style={{display:'block'}}>
-                            <p style={{display:'inline-block'}}>End date: </p><Moment format="LLL" date={this.props.data.SluttDatum} />
+                        <div style={{ display: 'block' }}>
+                            <p style={{ display: 'inline-block' }}>End date: </p><Moment format="LLL" date={this.props.data.SluttDatum} />
                         </div>
 
-                        <div style={{display:'block'}}>
+                        <div style={{ display: 'block' }}>
                             <progress value="50" max="100" />
                         </div>
-                        
-                        <div style={{marginTop: '2rem'}}>
-                        <div style={{display:'flex', flexDirection: 'row'}}>
-                            <form onSubmit={this.props.handleAddBid} style={{width:'100%'}}>
-                                <div style={{display:'flex', verticalAlign: 'middle', justifyContent:'space-between', alignItems:'flex-end'}}>
-                                    <div style={{display:'block'}}>
-                                        <label style={{paddingLeft:'0.5rem'}}>Name</label>
-                                        <input style={{flex:'1', marginRight:'1rem'}} className="form-control" type="text" placeholder="John Andres" name="name" />
+
+                        <div style={{ marginTop: '2rem' }}>
+                            <div style={{ display: 'flex', flexDirection: 'row' }}>
+                                <form onSubmit={this.props.handleAddBid} style={{ width: '100%' }}>
+                                    <div style={{ display: 'flex', verticalAlign: 'middle', justifyContent: 'space-between', alignItems: 'flex-end' }}>
+                                        <div style={{ display: 'block' }}>
+                                            <label style={{ paddingLeft: '0.5rem' }}>Name</label>
+                                            <input style={{ flex: '1', marginRight: '1rem' }} className="form-control" type="text" placeholder="John Andres" name="name" />
+                                        </div>
+                                        <div style={{ display: 'block' }}>
+                                            <label style={{ paddingLeft: '0.5rem' }}>Amount</label>
+                                            <input style={{ flex: '1', marginRight: '1rem' }} className="form-control" type="number" placeholder="100" name="amount" />
+                                        </div>
+                                        <div style={{ display: 'block', textAlign: 'end' }}>
+                                            <button style={{ display: 'block', verticalAlign: 'bottom' }} className="btn btn-success">Bid</button>
+                                        </div>
                                     </div>
-                                    <div style={{display:'block'}}>
-                                        <label style={{paddingLeft:'0.5rem'}}>Amount</label>
-                                        <input style={{flex:'1', marginRight: '1rem'}} className="form-control" type="number" placeholder="100" name="amount" />
-                                    </div>
-                                    <div style={{display:'block', textAlign: 'end'}}>
-                                        <button style={{display:'block', verticalAlign: 'bottom'}} className="btn btn-success">Bid</button>
-                                    </div>
-                                </div>
-                            </form>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                    </div>
-                    <div style={{background:'rgba(255,0,0,0.1', padding: '0.5rem', display:'flex', justifyContent: 'space-between', borderRadius: '0.3rem'}}>
-                        <button style={{margin:'0 0.5rem'}} className="btn btn-warning">Update</button>
+                    <div style={{ background: 'rgba(255,0,0,0.1', padding: '0.5rem', display: 'flex', justifyContent: 'space-between', borderRadius: '0.3rem' }}>
+                        <button style={{ margin: '0 0.5rem' }} className="btn btn-warning" onClick={this.handleClick}>Update</button>
                         <button className="btn btn-danger" onClick={() => this.props.handleDelete(this.props.data)}>Remove</button>
                     </div>
                 </div>
 
                 <div>
-                    <BidList selected={this.props.data}/>
+                    <BidList selected={this.props.data} />
                 </div>
-            </div>
+            </div>)
+
+        return (
+            this.state.displayAuction === true ? Content : <CreateAuction data={this.props.data} />
         )
     }
 }
