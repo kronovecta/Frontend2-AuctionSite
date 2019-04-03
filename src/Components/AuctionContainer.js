b9a9861e6abcab71a36aa01998b207f156038b2d
Schlebbe <sebastian@temtron.se>
2019-04-03 10:01:31 +0200

    Added toggle state to show SingleAuction

--- src/Components/AuctionContainer.js
+++ src/Components/AuctionContainer.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react'
 import AuctionList from './AuctionList'
 import { createSession } from '../api';
+import SingleAuction from './SingleAuction';
 
 export default class AuctionContainer extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      toggle: false,
+      auctionData: null
+    }
+  }
 
   componentDidMount() {
     createSession("auctionList", "auktion");
@@ -11,16 +20,24 @@ export default class AuctionContainer extends Component {
   handleAddBid = (e) => { // Generate new Bud into the API
     e.preventDefault();
     console.log(e.target.amount.value)
+
   }
 
   handleToggle = (data) => {
     console.log(data)
+    const newState = this.state.toggle === true ? false : true;
+    this.setState({
+      auctionData: data,
+      toggle: newState
+    })
   }
 
   render() {
     return (
       <React.Fragment>
-        <AuctionList handleAddBid={this.handleAddBid} searchString={this.props.searchString} handleToggle={this.handleToggle} />
+        {this.state.toggle === true ? <SingleAuction data = {this.state.auktionData} /> :
+          <AuctionList handleAddBid={this.handleAddBid} searchString={this.props.searchString} handleToggle={this.handleToggle} />
+        }
       </React.Fragment>
     )
   }
